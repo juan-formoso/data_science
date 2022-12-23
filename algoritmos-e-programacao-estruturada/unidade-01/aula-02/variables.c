fd539a78389c72110ee1084f2a3a56b1ef1a6d70
Juan Formoso <trajano.juan@gmail.com>
2022-12-23 14:48:47 -0400

    feat: :sparkles: Cria variables.c
    Adiciona código sobre declaração de variáveis.

---
+++ algoritmos-e-programacao-estruturada/unidade-01/aula-02/variables.c
@@ -0,0 +1,22 @@
+/*
+sintaxe: <tipo> <nome> = <valor>;
+
+tipos primitivos:
+int (inteiro)
+float (real) - precisão de 6 casas decimais
+double (real duplo) - precisão de 10 casas decimais
+char (caractere)
+bool (booleano) - precisa conter a biblioteca stdbool.h
+*/
+
+#include <stdbool.h>
+
+int main() {
+    int idade;
+    float salario = 1000.50;
+    double porcentagem_desconto = 0.1;
+    char genero = 'M';
+    bool estaAprovado = false;
+    return 0;
+}
+    
