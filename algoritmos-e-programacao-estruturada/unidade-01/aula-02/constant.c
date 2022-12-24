5ef2868f54785501f3dd23fa998a52bd6129bd59
Juan Formoso <trajano.juan@gmail.com>
2022-12-24 11:21:54 -0400

    feat: :sparkles: Cria arquivo constant.c
    Adiciona exercício sobre constantes.

---
+++ algoritmos-e-programacao-estruturada/unidade-01/aula-02/constant.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define PI 3.14 // rótulo
+
+int main() {
+  const float GRAVIDADE = 9.8;
+
+  printf("\n PI = %f", PI);
+  printf("\n GRAVIDADE = %f", GRAVIDADE);
+
+  return 0;
+}
