425627260910ac53d97e88c6a6dffb8cd097f3b0
Juan Formoso <trajano.juan@gmail.com>
2022-12-24 13:29:49 -0400

    feat: :sparkles: Cria arquivo vectors2.c
    Adiciona exercício de declaração de vetores usando colchetes.

---
+++ algoritmos-e-programacao-estruturada/unidade-01/aula-02/vectors2.c
@@ -0,0 +1,15 @@
+// Declaração de vetores usando colchetes
+#include <stdio.h>
+
+int main() {
+  float altura[3];
+  float altura[0] = 1.50;
+  float altura[1] = 1.80;
+  float altura[2] = 2.02;
+
+  printf("\n Vetor altura[0] = %f", altura[0]);
+  printf("\n Vetor altura[1] = %f", altura[1]);
+  printf("\n Vetor altura[2] = %f", altura[2]);
+
+  return 0;
+}
